d58270f4e9e912b0bb0d13bbc3b489f27cf7725e
Erik Wallin <erwa@Eriks-MacBook-Pro.local>
2022-05-06 07:09:51 +0200

    Fix menu. Fir firebase hosting

--- src/routes/__layout-unauthenticated.svelte
+++ src/routes/__layout-unauthenticated.svelte
@@ -4,19 +4,37 @@
 	import { getAuth, onAuthStateChanged } from 'firebase/auth';
 	import { user } from '$lib/stores/user';
 	import Menu from '$lib/components/menu.svelte';
-
 	import 'carbon-components-svelte/css/white.css';
+	import { query, getDocs, collection, where } from 'firebase/firestore';
+	import { db } from '$lib/fb';
+	import { leagues } from '$lib/stores/leagues';
+	import { browser } from '$app/env';
 
 	onMount(() => {
 		const auth = getAuth();
 		onAuthStateChanged(auth, (value) => {
 			if (value) {
 				user.set(value);
+				getLeagues();
 			} else {
                 user.set(undefined);
             }
 		});
 	});
+
+	async function getLeagues() {
+		if (browser) {
+			const querySnapshot = await getDocs(query(collection(db, "leagues"), where('owners', 'array-contains', $user.uid)));
+			const tempLeagues = [];
+			querySnapshot.forEach((doc) => {
+				tempLeagues.push({
+					id: doc.id, 
+					name: doc.data().name
+				});
+			});
+			leagues.set(tempLeagues);
+		}
+	}
 </script>
 
 <Menu />
---
+++ src/routes/index@unauthenticated.svelte
@@ -0,0 +1,37 @@
+<script>
+	import { Content, Grid, Row, Column, Link } from 'carbon-components-svelte';
+	
+	import { leagues } from '$lib/stores/leagues';
+	import { user } from '$lib/stores/user'; 
+</script>
+
+<Content>
+	<Grid>
+		<Row>
+			<Column>
+				<h1>Welcome to Dropin Tennis</h1>
+				<p>
+					Organizing friendly social singles and doubles. See <Link
+						inline
+						href="/leagues/about">rules</Link
+					> for gameplay.
+				</p>
+				{#if !$leagues && $user}
+				<p>
+					It is time to <Link
+						inline
+						href="/leagues/create">create</Link
+					> your first league.
+				</p>
+				{:else if $leagues && $user}
+				<p>
+					Select your league in the menu or <Link
+						inline
+						href="/leagues/create">create</Link
+					> another league.
+				</p>
+				{/if}
+			</Column>
+		</Row>
+	</Grid>
+</Content>
